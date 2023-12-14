0a3c1f7a4c02f4a357e7d749ce027dc3b922d544
Mateus Felipe <matheus@oialbert.com.br>
2023-12-14 17:27:52 -0300

    projeto-infinity

--- src/api/facematch.ts
+++ src/api/facematch.ts
@@ -5,15 +5,12 @@ const next = require('next')
 const dev = process.env.NODE_ENV !== 'production'
 const hostname = 'localhost'
 const port = 3000
-// when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port })
 const handle = app.getRequestHandler()
  
 app.prepare().then(() => {
-  createServer(async (req, res) => {
+  createServer(async (req: any, res:any) => {
     try {
-      // Be sure to pass `true` as the second argument to `url.parse`.
-      // This tells it to parse the query portion of the URL.
       const parsedUrl = parse(req.url, true)
       const { pathname, query } = parsedUrl
  
@@ -30,7 +27,7 @@ app.prepare().then(() => {
       res.end('internal server error')
     }
   })
-    .once('error', (err) => {
+    .once('error', (err: any) => {
       console.error(err)
       process.exit(1)
     })
